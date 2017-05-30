9077d440ad08bc9b498a1877ac8175eaa18d327b
TLA <TLAsm@DESKTOP-QV3JA3R>
2017-05-30 11:41:37 +0200

    Dingetjes toegevoegd

---
+++ MyWebApp/Demo/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWebApp.Demo
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string Surname { get; set; }
+
+    }
+}
